e8d0a84f43c12fa63ded5675b23f0d96fc419f33
Dmytro Oleksandryuk <dima.oleksandryuk@gmail.com>
2024-02-29 22:23:46 +0200

    Changed Structure

--- src/Creational/Builder/Program.cs
+++ src/Creational/Builder/Program.cs
@@ -6,6 +6,7 @@ ICarBuilder<Car> carBuilder = new CarBuilder();
 Car car = carBuilder.AddEngine()
     .AddDoor()
     .AddWheel()
+    .AddWheel()
     .Build();
 
 Console.WriteLine($"Car has engine: {car.Engine is not null}, wheels: {car.Wheels.Count}, doors: {car.Doors.Count}");
